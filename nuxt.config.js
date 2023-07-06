59bcc4ef8c011921e5f94da3ad14d2e6cf8dc02d
mukesh bhatia <mukeshbhatia@mukeshs-MacBook-Pro.local>
2023-07-07 03:50:42 +0530

    minor

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'final',
+    title: 'Hampshire-estates',
     htmlAttrs: {
       lang: 'en'
     },
