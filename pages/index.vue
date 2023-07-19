a2f24f4e56b60cb8f560cf6cdb2972f5f0429849
mukesh bhatia <mukeshbhatia@mukeshs-MacBook-Pro.local>
2023-07-19 11:56:11 +0530

    minor

--- pages/index.vue
+++ pages/index.vue
@@ -425,7 +425,7 @@
             <p class="font-normal lg:ml-12 text-left mt-2 text-[16px] lg:w-[97%] font-poppins tracking-normal leading-normal font-[Raleway]">
                
               • Shell Petrol Pump: 15 minutes <br>
-              • Gushing 5 star Hotels JW Marriott <br class="lg:hidden"/><span class="cust-mar">and Mulberry Shades by Marriott:</span></br class="lg:hidden"> <span class="cust-mar">20 minutes</span></br>
+              • Gushing 5 star Hotels JW Marriott <br class="lg:hidden"/><span class="cust-mar">and Mulberry Shades by Marriott:</span></br class="lg:hidden"/> <span class="cust-mar">20 minutes</span></br>
               • Close promixity to Railway Junction: <br class="lg:hidden"/>   <span class="cust-mar">Hindpur and Penukonda</span>.</br>
               • Golf Course: 20 minutes</br>
               • Starbucks: 15 minutes</br>
